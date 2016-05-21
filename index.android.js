df26020111b2f0bed65760c2931bcf45290ccb3a
Jeremy Colin <Jeremypa.colin@gmail.com>
2016-05-21 13:26:31 +0200

    Boilerplate setup

---
+++ index.android.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import reactNativeFirebaseStartApp from './js/index';
+
+AppRegistry.registerComponent('reactNativeFirebaseStartApp', () => reactNativeFirebaseStartApp);
